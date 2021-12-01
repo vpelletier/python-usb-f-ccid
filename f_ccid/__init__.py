3b27fc25bc1c616627fb045b3d724cda14935de9
Vincent Pelletier <plr.vincent@gmail.com>
2021-12-01 12:17:27 +0000

    Update versioneer to 0.21 .
    Hopefully fixes support for python 3.11 .

--- f_ccid/__init__.py
+++ f_ccid/__init__.py
@@ -466,6 +466,5 @@ class ICCDFunction(functionfs.Function):
         #elif message_type == MESSAGE_TYPE_SECURE: # No pin pad
         return (getErrorResponse(ERROR_CMD_NOT_SUPPORTED), )
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
